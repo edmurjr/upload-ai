27df9cfdc9df3205f60ba5f9fd559da2bd83ffcf
Edmur <edmur.serafim@gmail.com>
2023-09-14 18:19:17 -0300

    Finalização do Projeto

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -6,8 +6,32 @@ import { Label } from './components/ui/label'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select'
 import { Slider } from './components/ui/slider'
 import { VideoInputForm } from './components/video-input-form'
+import { PromptSelect } from './components/prompt-select'
+import { useState } from 'react'
+import { useCompletion } from 'ai/react'
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5)
+  const [videoId, setVideoId] = useState<string | null>(null)
+
+  const {
+    input,
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading,
+  } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+
   return (
     <div className='min-h-screen flex flex-col'>
       <div className="px-6 py-3 flex items-center justify-between border-b">
@@ -15,7 +39,7 @@ export function App() {
 
         <div className="flex items-center gap-3">
           <span className="text-sm text-muted-foreground">
-            Desenvolvido com 🩷 no NWL da Rocketseat
+            
           </span>
 
           <Separator orientation='vertical' className='h-6'/>
@@ -33,11 +57,14 @@ export function App() {
             <Textarea
               className='resize-none p-4 leading-relaxed'
               placeholder='Inclua o prompt para IA...'
+              value={input}
+              onChange={handleInputChange}
               />
             <Textarea 
               className='resize-none p-4 leading-relaxed'
               placeholder='Resultado gerado pela IA...'
               readOnly
+              value={completion}
             />
           </div>
 
@@ -47,22 +74,14 @@ export function App() {
         </div>
 
         <aside className='w-80 space-y-6'>
-          <VideoInputForm />
+          <VideoInputForm onVideoUploaded={setVideoId}/>
 
           <Separator />
 
-          <form className='space-y-6'>
+          <form onSubmit={handleSubmit} className='space-y-6'>
             <div className='space-y-2'>
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder='Selecione um prompt...'/>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value='title'>Título do YouTube</SelectItem>
-                  <SelectItem value='description'>Descrição do YouTube</SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={setInput}/>
             </div>
 
             <div className='space-y-2'>
@@ -86,6 +105,8 @@ export function App() {
                 min={0}
                 max={1}
                 step={0.1}
+                value={[temperature]}
+                onValueChange={value => setTemperature(value[0])}
               />
               <span className='block text-xs text-muted-foreground italic leading-relaxed select-none'>
                 Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros.
@@ -94,7 +115,11 @@ export function App() {
             
             <Separator />
 
-            <Button type='submit' className='w-full'>
+            <Button
+              disabled={isLoading}
+              type='submit'
+              className='w-full'
+            >
               Executar
               <Wand2 className='m-4 h-4 ml-2'/>
             </Button>
